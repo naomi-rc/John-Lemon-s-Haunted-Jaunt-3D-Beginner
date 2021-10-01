ea8cba6a9a10bc526ac616e6deb6f4aea2b054a3
naomi-rc <35537740+naomi-rc@users.noreply.github.com>
2021-10-01 00:51:25 -0400

    completed audio and build tutorials

--- John Lemon Hauted Jaunt/Assets/Scripts/GameEnding.cs
+++ John Lemon Hauted Jaunt/Assets/Scripts/GameEnding.cs
@@ -8,9 +8,12 @@ public class GameEnding : MonoBehaviour
     public float fadeDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource caughtAudio;
     public float displayImageDuration = 1f;
     float m_Timer;
+    bool m_HasAudioPlayed;
     bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
 
@@ -32,16 +35,22 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel(exitBackgroundImageCanvasGroup, false);
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
         }
         else if (m_IsPlayerCaught)
         {
-            EndLevel(caughtBackgroundImageCanvasGroup, true);
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     }
 
-    private void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+    private void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
     {
+        if(!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
         if(m_Timer > fadeDuration + displayImageDuration)
--- John Lemon Hauted Jaunt/Assets/Scripts/PlayerMovement.cs
+++ John Lemon Hauted Jaunt/Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,13 @@ public class PlayerMovement : MonoBehaviour
     Vector3 m_Movement; //'m' for member variable, non-public variable    
     Quaternion m_Rotation = Quaternion.identity;
     Rigidbody m_RigidBody;
+    AudioSource m_AudioSource;
 
     void Start()
     {
         m_Animator = GetComponent<Animator>(); //gets GameObject's Animator component (an instance)
         m_RigidBody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
@@ -29,6 +31,18 @@ public class PlayerMovement : MonoBehaviour
 
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
+
+        if(isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }            
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
